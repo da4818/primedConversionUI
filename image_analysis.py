720ca802a86bdd043cf2bb40f9173a8583b313dc
da4818 <diadegoke@gmail.com>
2021-01-28 22:42:01 +0000

    Added buttons in grid format - change window title Image added into windows.py Imported functions from different .py file

--- image_analysis.py
+++ image_analysis.py
@@ -1,8 +1,40 @@
-#import matplotlib
+import PIL
+from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 #from networkx.drawing.tests.test_pylab import plt
 #from skimage import data
 
-# We will use this pythong script to perform the image analysis and display the results
-def sendMessage():
+# We will use this python script to perform the image analysis and display the results
+
+
+
+def send_message():
     return 'Example text'
 
+photo = PIL.Image.open("test4.tif")
+photo_rgb = photo.convert("RGB")
+width, height = photo.size
+#rgb_val = photo_rgb.getpixel((1,1))
+R0,G0,B0 = photo_rgb.getpixel((0,0))
+print("size:",width,",",height)
+print("Values:",R0,G0,B0)
+#print(rgb_val)
+for x in range(width):
+    for y in range(height):
+        R,G,B = photo_rgb.getpixel((x,y))
+        new_R,new_G,new_B = (R-R0)*50,(B-B0)*50,(G-G0)*50 # Multiplying by 50 amplifies the difference in brightness
+        #print(D,E,F)
+        #R1,G1,B1 = photo_rgb.getpixel((x,y))
+        #R1, G1, B1 = photo_rgb.getpixel((x,y))
+        photo_rgb.putpixel((x,y),(new_R,new_G,new_B))
+#photo_rgb.save('example'+str(3  )+'.png')
+#photo.show()
+photo_rgb.show()
+def get_file(i):
+    out = 'example'+str(i)+'.png'
+    #photo_rgb.save(photo_rgb.save('example'+str(1)+'.png'))
+    return out
+
+#example3.png is the edited file "pr-mEosFP new_pr-mEosFP_new_before_4_ch00.tif
--- windows.py
+++ windows.py
@@ -1,6 +1,11 @@
 import tkinter as tk
-from tkinter import *
-from image_analysis import sendMessage # We can use this to use functions from other python scripts
+from tkinter import ttk, Label, Button, PhotoImage
+
+import PIL
+from PIL import Image, ImageTk
+from PIL.ImageTk import PhotoImage
+
+from image_analysis import send_message, get_file # We can use this to use functions from other python scripts
 
 # Setting up the initial window
 window = tk.Tk()
@@ -8,31 +13,53 @@ window.title('Home')
 window.geometry("600x600")
 
 def display_primed_conversion():
-    pr_window = tk.Tk()
+    pr_window = tk.Toplevel() # Widget that is daughter window of the parent
     pr_window.title('Primed Conversion Testing Stage')
     pr_window.geometry("600x600")
-    output = Label(pr_window, text=sendMessage())
-    output.place(x=100, y=100)
+    canvas = tk.Canvas(pr_window, width = 500, height = 400)
+    canvas.pack()
+    image = Image.open(get_file(3)) # This function returns the name of the png file to be imported
+    image = image.resize((476, 378), Image.ANTIALIAS)
+    img = PhotoImage(image)
+    canvas.create_image(12, 11, anchor=tk.NW, image=img) #(500-476)/2=12, (400-378)/2=11
+    pr_window.mainloop()
+
+#output = Label(pr_window, text=send_message())
+    #output.place(x=100, y=100)
 # When Primed Conversion button is pressed, it will display "Example code" to the window as a label
 
 def display_photo_conversion():
-    pc_window = tk.Tk()
+    pc_window = tk.Toplevel()
     pc_window.title('Photo Conversion Testing Stage')
     pc_window.geometry("600x600")
 
 def display_excitation():
-    e_window = tk.Tk()
+    e_window = tk.Toplevel()
     e_window.title('Excitation Testing Stage')
     e_window.geometry("600x600")
+    e_window.columnconfigure(0, weight=1, minsize=75)
+    e_window.columnconfigure(1, weight=1, minsize=75)
+    e_window.rowconfigure(0, weight=1, minsize=50)
+    e_window.rowconfigure(1, weight=1, minsize=75)
+    e_window.rowconfigure(2, weight=1, minsize=50)
+    button_red = Button(e_window, text="Red Excitation",)
+    button_green = Button(e_window, text="Green Excitation")
+    e_frame = tk.Frame(
+        master = e_window,
+        relief = tk.RAISED,
+        borderwidth = 1
+    )
+    e_frame.grid(row=0, column=0, padx=5, pady=5)
+    button_red.grid(row=0, column=0)
+    e_frame.grid(row=0, column=1, padx=5, pady=5)
+    button_green.grid(row=0, column=1)
+    e_window.mainloop()
+
+
 
 b1 = Button(window, text="Regular Excitation", command=display_excitation)
 b2 = Button(window, text="Photo Conversion", command=display_photo_conversion)
 b3 = Button(window, text="Primed Conversion", command=display_primed_conversion)
-
-#b1.grid(row=0, column=0)
-#b2.grid(row=1, column=1)
-#b3.grid(row=1, column=2)
-
 # This produces a grid structure so we can add the buttons in a grid format
 window.columnconfigure(0, weight=1, minsize=75)
 window.rowconfigure(0, weight=1, minsize=50)
