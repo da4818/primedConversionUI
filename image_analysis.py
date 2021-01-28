793f2b92f03eb719aced3050f1d842662bd2a85d
da4818 <diadegoke@gmail.com>
2021-01-28 17:03:41 +0000

    Added buttons in grid format - change window title Imported functions from different .py file

---
+++ image_analysis.py
@@ -0,0 +1,8 @@
+#import matplotlib
+#from networkx.drawing.tests.test_pylab import plt
+#from skimage import data
+
+# We will use this pythong script to perform the image analysis and display the results
+def sendMessage():
+    return 'Example text'
+
--- windows.py
+++ windows.py
@@ -1,18 +1,29 @@
 import tkinter as tk
 from tkinter import *
+from image_analysis import sendMessage # We can use this to use functions from other python scripts
+
+# Setting up the initial window
 window = tk.Tk()
 window.title('Home')
-window.geometry("400x400")
+window.geometry("600x600")
 
 def display_primed_conversion():
-    window.title('Primed Conversion Testing Stage')
+    pr_window = tk.Tk()
+    pr_window.title('Primed Conversion Testing Stage')
+    pr_window.geometry("600x600")
+    output = Label(pr_window, text=sendMessage())
+    output.place(x=100, y=100)
+# When Primed Conversion button is pressed, it will display "Example code" to the window as a label
 
 def display_photo_conversion():
-    window.title('Photo Conversion Testing Stage')
+    pc_window = tk.Tk()
+    pc_window.title('Photo Conversion Testing Stage')
+    pc_window.geometry("600x600")
 
 def display_excitation():
-    window.title('Excitation Testing Stage')
-
+    e_window = tk.Tk()
+    e_window.title('Excitation Testing Stage')
+    e_window.geometry("600x600")
 
 b1 = Button(window, text="Regular Excitation", command=display_excitation)
 b2 = Button(window, text="Photo Conversion", command=display_photo_conversion)
@@ -22,6 +33,7 @@ b3 = Button(window, text="Primed Conversion", command=display_primed_conversion)
 #b2.grid(row=1, column=1)
 #b3.grid(row=1, column=2)
 
+# This produces a grid structure so we can add the buttons in a grid format
 window.columnconfigure(0, weight=1, minsize=75)
 window.rowconfigure(0, weight=1, minsize=50)
 window.columnconfigure(1, weight=1, minsize=75)
@@ -29,9 +41,9 @@ window.rowconfigure(1, weight=1, minsize=50)
 window.columnconfigure(2, weight=1, minsize=75)
 window.rowconfigure(2, weight=1, minsize=50)
 frame = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1
+    master = window,
+    relief = tk.RAISED,
+    borderwidth = 1
 )
 frame.grid(row=1, column=0, padx=5, pady=5)
 b1.grid(row=2, column=0)
