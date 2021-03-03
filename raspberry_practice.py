9f99832d56860398f3684eb26e3d61ee97227f5b
da4818 <diadegoke@gmail.com>
2021-03-03 18:26:23 +0000

    Added testing program

---
+++ raspberry_practice.py
@@ -0,0 +1,8 @@
+from raspigpio import *
+
+if __name__ == "__main__":
+    raspi_connection('UV')
+    raspi_connection('PC')
+    raspi_connection('green_excitation')
+    raspi_connection('red_excitation')
+
