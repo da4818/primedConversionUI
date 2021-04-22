caba81aaa31c1a57eca61c3f894292a7544faa78
da4818 <diadegoke@gmail.com>
2021-04-22 23:24:39 +0100

    added comments

--- profile_graph.py
+++ profile_graph.py
@@ -7,20 +7,27 @@ img = cv2.imread("pre_pr_red1.png")
 colour_mask = cv2.imread("pre_pr_red1.png")
 width=img.shape[0]
 
-
+#creates array to determine area of image that is to be masked
 mask = np.zeros(img.shape[:2], np.uint8)
+#the chosen masking area is a 1px thick line from x=100 to x=400
 mask[200:201, 100:400] = 255
-
+#creates vector of brightness for chosen pixels (typed in manually)
 pixel_brightnesses = img[100:400,220]
+#creates vector of the pixel location (typed in manually)
 pixel_locations = np.arange(100,400)
 
-
+#this masks off everything outside of the masking range (plot 3)
+# -->sets everything else to black (since it's 1px thick it's hard to see)
 masked_img = cv2.bitwise_and(img,img, mask = mask)
 colour_mask[200:201, 100:400] = (255,255,255)
+#original image - not too sure why the image displays as blue
 plt.subplot(221), plt.imshow(img)
+#original image with profile section highlighted in white
 plt.subplot(222), plt.imshow(colour_mask)
 #plt.subplot(222), plt.imshow(mask,'gray')
+#masked image
 plt.subplot(223), plt.imshow(masked_img)
+#4th plot is the brightness profile
 plt.subplot(224), plt.plot(pixel_locations,pixel_brightnesses)
 plt.xlim([0,width])
 
