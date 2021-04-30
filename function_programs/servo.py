09d31372459fd03e1bc76e1c9d7192f773cee233
da4818 <diadegoke@gmail.com>
2021-04-30 19:44:41 +0100

    updated servo code using gpiozero

--- function_programs/servo.py
+++ function_programs/servo.py
@@ -1,44 +1,23 @@
 
-#import RPi.GPIO as GPIO --> error installing package
-'''Motor angle calculated as percentage of the pulse cycle - called duty cycle (in %)
-1ms signal corresponds to -90 deg, 1.5ms signal corresponds to 0deg, 2ms corresponds to 90
-A servo uses a 50Hz control signal - period is 20ms per cycle
-Therefore to obtain anticlockwise, the duty cycle must be 1/20= 5%, middle duty cycle = 7.5%, clockwise = 10%'''
-
-#We will need to test these values, as it may vary between servos, generally 4% grants full left, 12.5% grants full right
-
-# Class to connect and initiate rotation, then turn off - I may change this to be turned on and silently waiting for signal
-class servo:
+from gpiozero import Servo
+class motor:
     def __init__(self, filter_colour):
-        self.filter_colour = filter_colour
-        '''GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
-        GPIO.setwarnings(False) #Disable warnings
-        servo_pin = 12 #Pin number for PWM signal- can be connect to 7, 11, 12, 13, 15, 16, 16 & 22
-        frequency = 50 #50Hz - 20ms pulse width/cycle - value used in servos
-        GPIO.setup(servo_pin, GPIO.OUT)
-        pwm = GPIO.PWM(servo_pin, frequency)
-        self.set_filter(filter_colour)'''
+        servo = Servo(17) #GPIO17 = BOARD11
+        self.servo.value = 0
+        self.servo.value = None #Will need to ensure the rwheel is set properly when putting it into the housing
+        self.set_filter(filter_colour)
 
-        #Close GPIO & cleanup - it should keep the motor in the previous position
-        '''pwm.stop()
-        GPIO.cleanup()'''
 
     def set_filter(self, filter_colour):
         if filter_colour == "green":
-            self.pwm.start(angle_to_percent(0))
+            self.servo.value = 0
             print("Set to green filter")
         elif filter_colour == "red":
-            self.pwm.start(angle_to_percent(180))
+            self.servo.value = 0.5454545454 #value to rotate 30 degrees (from 0 to 1 is 55 degrees rotation)
             print("Set to red filter")
+        self.servo.detach()
 
-def angle_to_percent (angle):
-    if angle > 180 or angle < 0 :
-        return False
-    start = 4
-    end = 12.5
-    ratio = (end - start)/180 #Calculate ratio from angle to percent
-    angle_as_percent = angle * ratio
-    return start + angle_as_percent
+
 
 
 
