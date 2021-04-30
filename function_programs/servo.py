ab24dffff19206556c6c3c511b94ba07a5f2549a
da4818 <diadegoke@gmail.com>
2021-04-30 19:46:06 +0100

    updated servo code using gpiozero

--- function_programs/servo.py
+++ function_programs/servo.py
@@ -2,11 +2,10 @@
 from gpiozero import Servo
 class motor:
     def __init__(self, filter_colour):
-        servo = Servo(17) #GPIO17 = BOARD11
+        self.servo = Servo(17) #GPIO17 = BOARD11
         self.servo.value = 0
-        self.servo.value = None #Will need to ensure the rwheel is set properly when putting it into the housing
+        self.servo.value = None #Will need to ensure the wheel is set properly when putting it into the housing
         self.set_filter(filter_colour)
-
 
     def set_filter(self, filter_colour):
         if filter_colour == "green":
@@ -16,9 +15,3 @@ class motor:
             self.servo.value = 0.5454545454 #value to rotate 30 degrees (from 0 to 1 is 55 degrees rotation)
             print("Set to red filter")
         self.servo.detach()
-
-
-
-
-
-
