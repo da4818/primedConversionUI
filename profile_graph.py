bbe08547efc22d1915a5e8d5569e3019837378fa
da4818 <diadegoke@gmail.com>
2021-05-11 18:04:35 +0100

    incorporating profile graph onto tkinter

--- profile_graph.py
+++ profile_graph.py
@@ -7,53 +7,36 @@ img = cv2.imread("pre_pr_red1.png")
 colour_mask = cv2.imread("pre_pr_red1.png")
 width=img.shape[0]
 
+
+#automating profile variables
+start_x=0
+end_x=width
+y=200
 #creates array to determine area of image that is to be masked
 mask = np.zeros(img.shape[:2], np.uint8)
 #the chosen masking area is a 1px thick line from x=100 to x=400
-mask[200:201, 100:400] = 255
-#creates vector of brightness for chosen pixels (typed in manually)
-pixel_brightnesses = img[100:400,220]
-#creates vector of the pixel location (typed in manually)
-pixel_locations = np.arange(100,400)
+mask[y:y+1, start_x:end_x] = 255
+#creates matrix of brightness for chosen pixels (forms a 3 column matrix - the first 2 column are irrelevant)
+pixel_brightnesses = img[start_x:end_x,y]
+#creates vector of the pixel location selection (e.g., x coord 1,2,3....)
+pixel_locations = np.arange(start_x,end_x)
 
 #this masks off everything outside of the masking range (plot 3)
 # -->sets everything else to black (since it's 1px thick it's hard to see)
 masked_img = cv2.bitwise_and(img,img, mask = mask)
-colour_mask[200:201, 100:400] = (255,255,255)
+colour_mask[y:y+1, start_x:end_x] = (255,255,255)
+
 #original image - not too sure why the image displays as blue
 plt.subplot(221), plt.imshow(img)
 #original image with profile section highlighted in white
 plt.subplot(222), plt.imshow(colour_mask)
-#plt.subplot(222), plt.imshow(mask,'gray')
 #masked image
-plt.subplot(223), plt.imshow(masked_img)
-#4th plot is the brightness profile
-plt.subplot(224), plt.plot(pixel_locations,pixel_brightnesses)
+'''plt.subplot(223), plt.imshow(masked_img)
+#4th plot is the brightness profile'''
+plt.subplot(224), plt.plot(pixel_brightnesses[:,2])
+#as the columns 0 and 1 are irrelevant, we want to only take information for column 2
 plt.xlim([0,width])
-
-
-
 
 plt.show()
 
 
-'''for x in range(10):
-    print(img.getpixel((x+100, 50)))
-    img.putpixel((x+100, 50), (255, 255, 255))
-#grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.hist(img.ravel(),256,[0,256]); plt.show()'''
-
-#hist, bins = np.histogram(img.ravel(),256,[0,256])
-#hist = cv2.calcHist([img],[0],None,[256],[0,256])
-
-
-'''image = Image.open("pre_pr_red1.png")
-im = image.convert('L')
-width, height = image.size
-#image = skimage.io.imread(filename, as_gray=True)
-print(width, height)
-
-for x in range(10):
-    print(im.getpixel((x+100, 50)))
-    image.putpixel((x+100, 50), (255, 255, 255))
-image.show()'''
--- windows.py
+++ windows.py
@@ -4,23 +4,18 @@ from tkinter import *
 from tkinter.ttk import Frame, Button, Entry
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg#, NavigationToolbar2Tk
+import skimage.io
+from image_profile import *
 import PIL
 from PIL import Image, ImageTk
 from PIL.Image import ANTIALIAS
-from test_files.skimage_image_analysis import get_files
-from function_programs.image_analysis import *
-from function_programs.files import *
+
 root = Tk()
 root.title("Primed Conversion Testing Stage")
 root.geometry("650x600")
-'''
-Buttons are displayed in order of 
-'Home','Regular Excitation','Photo Conversion','Primed Conversion', 'Load Previous Data'
-'''
 
 #START PAGE
 class startPage(Frame):
-
     def __init__(self):
         super().__init__()
         self.master.title("Home")
@@ -28,200 +23,38 @@ class startPage(Frame):
         frame.pack(fill="both", expand=True)
         self.pack(fill="both", expand=True)
 
-        excitationButton = Button(self, text="Regular Excitation", command=lambda: (self.destroy(), excitationPage()))
-        excitationButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        photoButton = Button(self, text="Photo Conversion", command=lambda: (self.destroy(), photoPage()))
-        photoButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        primedButton = Button(self, text="Primed Conversion", command=lambda: (self.destroy(), primedPage()))
-        primedButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        dataButton = Button(self, text="Load Previous Data", command=lambda: (self.destroy(), dataPage()))
-        dataButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-
-#EXCITATION PAGE
-class excitationPage(Frame):
-
-    def __init__(self):
-        super().__init__()
-        self.master.title("Regular Excitation")
-        exFrame = Frame(self, relief=RAISED, borderwidth=1)
-        exFrame.pack(fill="both", expand=True)
-
-        redButton = Button(exFrame, text="Red Excitation", command=lambda: (self.destroy(), colourExcitationPage("red")))
-        redButton.pack(side="top", fill="both", expand=True, padx=5, pady=10)
-        greenButton = Button(exFrame, text="Green Excitation",
-                             command=lambda: (self.destroy(), colourExcitationPage('green')))
-        greenButton.pack(side="top", fill="both", expand=True, padx=5, pady=10)
-
-        self.pack(fill="both", expand=True)
-
-        home = Button(self, text="Home", command=lambda: (self.destroy(), startPage()))
-        home.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        photoButton = Button(self, text="Photo Conversion", command=lambda: (self.destroy(), photoPage())) #Closes the current page and calls the next page to appear within the same frame
-        photoButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        primedButton = Button(self, text="Primed Conversion", command=lambda: (self.destroy(), primedPage()))
-        primedButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        dataButton = Button(self, text="Load Previous Data", command=lambda: (self.destroy(), dataPage()))
-        dataButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-
-
-'''
-The red and green excitation will perform similar commands, 
-but will vary in terms of the title name and the type of light to turn on and off
-Here, a validation check is used to confirm whether red or green excitation occurs - 
-appropriate functions will be added accordingly
-'''
-class colourExcitationPage(Frame):
-    def __init__(self, colour):
-        super().__init__()
-        if colour == 'red_excitation':
-            print('Red Excitation')
-            self.master.title("Red Excitation - (Post PC)")
-        elif colour == 'green_excitation':
-            print('Green Excitation')
-            self.master.title("Green Excitation - (Pre PC)")
-
-        frame = Frame(self, relief="raised", borderwidth=1)
-        frame.pack(fill="both", expand=True)
-        self.pack(fill="both", expand=True)
-        #, frame.after(4000, analysisPage(frame, "sample.png"))
-        startButton = Button(self, text="Start Excitation", command=lambda: (display_LED_message(self, frame))) #Closes the current page and calls the next page to appear within the same frame
-        startButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        stopButton = Button(self, text="Stop")
-        stopButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        backButton = Button(self, text="Back", command=lambda: (self.destroy(), excitationPage()))
-        backButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        #Closes the current page and calls the next page to appear within the same frame
-
-#PRIMED CONVERSION PAGE
-class primedPage(Frame):
-    def __init__(self):
-        super().__init__()
-        self.master.title("Primed Conversion")
-        prFrame = Frame(self, relief=RAISED, borderwidth=1)
-        prFrame.pack(fill="both", expand=True)
-        self.pack(fill="both", expand=True)
-
-        startButton = Button(prFrame, text="Start Primed Conversion")
-        startButton.pack(fill="both", expand=True, padx=5, pady=5)
-        home = Button(self, text="Home", command=lambda: (self.destroy(), startPage()))
-        home.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        excitationButton = Button(self, text="Regular Excitation", command=lambda: (self.destroy(), excitationPage()))
-        excitationButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        photoButton = Button(self, text="Photo Conversion", command=lambda: (self.destroy(), photoPage()))
-        photoButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        dataButton = Button(self, text="Load Previous Data", command=lambda: (self.destroy(), dataPage()))
-        dataButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-#PHOTO CONVERSION PAGE
-class photoPage(Frame):
-    def __init__(self):
-        super().__init__()
-        self.master.title("Photo Conversion")
-        pcFrame = Frame(self, relief=RAISED, borderwidth=1)
-        pcFrame.pack(fill="both", expand=True)
-        self.pack(fill="both", expand=True)
-        startButton = Button(pcFrame, text="Start Photo Conversion", command=lambda: (startButton.forget(), analysisPage(pcFrame,"sample.png")))
-        startButton.pack(fill="both", expand=True, padx=5, pady=5)
-
-        home = Button(self, text="Home", command=lambda: (self.destroy(), startPage()))
-        home.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        excitationButton = Button(self, text="Regular Excitation", command=lambda: (self.destroy(), excitationPage()))
-        excitationButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        primedButton = Button(self, text="Primed Conversion", command=lambda: (self.destroy(), primedPage()))
-        primedButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        dataButton = Button(self, text="Load previous data", command=lambda: (self.destroy(), dataPage()))
-        dataButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-#PREVIOUS DATA PAGE
-class dataPage(Frame):
-    def __init__(self):
-        super().__init__()
-        self.master.title("Previous Data")
-        dataFrame = Frame(self, relief=RAISED, borderwidth=1)
-        dataFrame.pack(fill="both", expand=True)
-
-        imageinfo = get_files() #Will modify this to use files.py
-        canvas = tk.Canvas(dataFrame, width = 300, height = 500, bg='gray92')
-        canvas.pack(fill="both", expand=True, pady=5)
-        for i in range(len(imageinfo)):
-            photo = PIL.Image.open(os.path.join(imageinfo[i].path, imageinfo[i].name)).resize((150, 150), ANTIALIAS)
-            render = ImageTk.PhotoImage(photo)
-            img = Label(canvas, text="Test "+str(i+1), image=render, compound="bottom")
-            img.image = render
-            img.pack(side="left", anchor=NW, fill="none", expand=True, padx=5, pady=5)
-
-        self.pack(fill="both", expand=True)
-        home = Button(self, text="Home", command=lambda: (self.destroy(), startPage()))
-        home.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        excitationButton = Button(self, text="Regular Excitation", command=lambda: (self.destroy(), excitationPage()))
-        excitationButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        primedButton = Button(self, text="Primed Conversion", command=lambda: (self.destroy(), primedPage()))
-        primedButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-        photoButton = Button(self, text="Photo Conversion", command=lambda: (self.destroy(), photoPage()))
-        photoButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+        analysisButton = Button(self, text="Load Previous Data", command=lambda: (self.destroy(), analysisPage()))
+        analysisButton.pack(side="left", fill="both", expand=True, padx=5, pady=5)
 
 #DATA ANALYSIS PAGE
 class analysisPage(Frame):
-    def __init__(self, frame, filename):
+    def __init__(self):
         super().__init__()
         self.master.title("Image Analysis")
-        d = 25
-        img, img1 = export_images(filename)
+        self.filename="pre_pr_red1.png"
+
+        frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame.pack(fill="both", expand=True)
+        self.pack(fill="both", expand=True)
         fig = plt.figure(constrained_layout=True)
         spec = fig.add_gridspec(2, 2)
+
+        img, brightness_profile = generate_brightness_profile(self.filename)
         a = fig.add_subplot(spec[0, 0])
         a.imshow(img)
         a.axis('off')
         a.set_title("Normalised")
-        b = fig.add_subplot(spec[0, 1])
-        b.imshow(img1)
-        b.axis('off')
-        b.set_title("Masked")
         c = fig.add_subplot(spec[1, 0:2])
-        self.show_graph(c, fig, 25, filename)
+        c.plot(brightness_profile[:,2])
+        c.set_xlabel('Pixel location')
+        c.set_ylabel('Brightness')
+        c.set_title("Graph")
+        fig.canvas.draw()
+
         canvas = FigureCanvasTkAgg(fig, frame)
 
         plot_widget = canvas.get_tk_widget()
         plot_widget.pack(side="top", fill="both", expand=True, padx=5, pady=5)
-
-    def show_graph(self, c, fig, distance, filename):
-        thresholds, colours = get_thresholds()
-        hist, bin_edges = generate_histogram(filename)
-        c.plot(bin_edges[0:-1], hist)
-
-        for t, col in zip(thresholds, colours):
-            c.axvline(x=t, color=col, label='line at x = {}'.format(t))
-
-        c.set_xlabel('Greyscale value')
-        c.set_ylabel('Number of pixels')
-        c.set_title("Graph")
-        fig.canvas.draw()
-
-
-    def submit(self, number, c, fig, d):
-        if only_numbers(number.get()):
-            d = int(number.get()) #the smaller the number, the more peaks or detected
-            print(d)
-            c.cla()
-            self.show_graph(c, fig, d, "sample.png")
-        else:
-            print("Invalid entry, try again")
-
-def only_numbers(char):
-    return char.isdigit()
-
-def message(self, frame, label):
-    label['text'] = "LED off..."
-    frame.after(1000, remove_message, self, label, frame)
-
-def remove_message(self, label, frame):
-    label.forget()
-    #self.destroy()
-    analysisPage(frame, "sample.png")
-
-def display_LED_message(self, frame):
-    label = Label(frame, text="LED on...", bg='gray92')
-    label.pack()
-    frame.after(1000, message, self, frame, label)
-
 
 if __name__ == "__main__":
     startPage()
